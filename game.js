3c89c810a5d877fc375768f9ea0c9131569f4dfc
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-09-08 22:29:44 -0400

    starting game.js file

---
+++ game.js
@@ -0,0 +1,14 @@
+kaboom({
+  global: true,
+  fullscreen: true,
+  scale: 1,
+  debug: true,
+  clearColor: [0, 0, 1, 1],
+});
+
+//speeds
+const MOVE_SPEED = 120;
+const SLICER_SPEED = 100;
+const SKELETOR_SPEED = 60;
+
+//game logic
